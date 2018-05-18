e579a706fded14cc29e471372b04011649eb4bff
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 20:31:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime?(integer)
 x = 2
     return false if integer < 2
+    if integer % 2 == 0
+      return false
 while x < integer
           if (integer % x) == 0
              return false
